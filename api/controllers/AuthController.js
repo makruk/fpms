5ecf7eddeae5f16b3b5b430dbe83bb0a40a001b2
makruk <tetsumasax@hotmail.com>
2014-06-11 21:36:08 +0900

    ログイン処理追加
    暗号化・セッションの細かい設定は未確定

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -6,5 +6,30 @@
  */
 
 module.exports = {
-	
+  login:function(req, res){
+    var user_id=req.param("id");
+    var password=req.param("password");
+    if(user_id !== void 0){
+      User.find({user_id:user_id}).exec(function(err, found){
+        if( found[0] !== void 0 && found[0].password === password ){
+          req.session.user_id=user_id;
+          req.session.permission=found[0].permission;
+
+          /*リダイレクト先は後で変更*/
+          res.redirect("/request/");
+        }
+        else{
+          res.view();
+        }
+      });
+    }
+    else{
+      res.view();
+    }
+  },
+  logout:function(req, res){
+    req.session.user_id=void 0;
+    req.session.permission=void 0;
+    res.redirect("/auth/login");
+  }
 };
