6852c578843555ea61d75fa6245cad8ab6430894
makruk <tetsumasax@hotmail.com>
2014-06-11 20:44:10 +0900

    generate model

---
+++ api/models/Request.js
@@ -0,0 +1,16 @@
+/**
+* Request.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    request:{type:"string"},
+    responce:{type:"string"},
+    review:{type:"integer"}
+  }
+};
+
---
+++ api/models/User.js
@@ -0,0 +1,20 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    name:{type:"string"},
+    user_id:{type:"string"},
+    password:{type:"string"},
+    grade:{type:"integer"},
+    balance:{type:"integer"},
+    permission:{type:"integer"},
+    limit:{type:"integer"}
+  }
+};
+
