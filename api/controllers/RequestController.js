e57c0b8081ca12a48c8833e4b6e148f89cd11d33
MYAX <myaxsan@gmail.com>
2014-06-12 19:12:52 +0900

    リクエスト関連ｎ

--- api/controllers/RequestController.js
+++ api/controllers/RequestController.js
@@ -9,18 +9,20 @@ module.exports = {
 	
   index:function(req, res){
 		Request.find({}).exec(function(err,found){
-			this.request = found;
+			this.requests = found;
 		});
     return res.view();
   },
   request:function(req, res){
-			this.request = found;
+		Request.find({}).exec(function(err,found){
+			this.requests = found;
+		});
 	  return res.view();
   },
 	submit:function(req,res){
 		var request = req.param('request');
 		Request.create({request:request,review:0})
 		.exec(function(err){});
-		return res.redirect("/request")
+		return res.redirect("/request/request");
 	}
 };
