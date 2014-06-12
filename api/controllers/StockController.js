c28665c083efd25e28fc5e975d57f7c587a32a94
garubangoru <imdeadender@yahoo.co.jp>
2014-06-12 17:53:38 +0900

    StockController

--- api/controllers/StockController.js
+++ api/controllers/StockController.js
@@ -8,9 +8,18 @@
 module.exports = {
 	
   index:function(req, res){
+		Stock.find({}).exec(function(err,found){
+			this.stock = found;
+		});
     return res.view();
   },
   stock:function(req, res){
-    return res.view();
-  }
+	  return res.view();
+  },
+	submit:function(req,res){
+		var stock = req.param('stock');
+		Stock.create({stock:stock,review:0,evaluation:0})
+		.exec(function(err){});
+		return res.redirect("/stock")
+	}
 };
