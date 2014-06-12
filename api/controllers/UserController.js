c25c21bb06c37a13a49e22fa2c921cd55b0c58d0
chikyu <11t5088g@gmail.com>
2014-06-12 18:30:35 +0900

    edit user

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -14,6 +14,13 @@ module.exports = {
     return res.view();
   },
   create:function(req, res){
-  	return res.view();
+  	var name=req.param('name');
+	var user_id=req.param('user_id');
+	var password=req.param('password');
+	var grade=req.param('grade');
+	var permission=req.param('permission');
+	var limit=req.param('limit');
+	User.create({name:name, user_id:user_id, password:password, grade:grade, permission:permission, limit:limit}).exec(function(err){});
+  	return res.redirect("/user");
   }
 };
