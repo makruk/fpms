4c20f1352bc18b30b22650664958620800dfaa89
chikyu <11t5088g@gmail.com>
2014-06-16 17:13:02 +0900

    検索

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -8,6 +8,11 @@
 module.exports = {
 	
   index:function(req, res){
+    var name=req.param('name') || "";
+    User.find({name:name}).exec(function findCB(err,found){
+      this.found=found;
+   }
+  );
     return res.view();
   },
   user:function(req, res){
@@ -15,12 +20,13 @@ module.exports = {
   },
   create:function(req, res){
   	var name=req.param('name');
-	var user_id=req.param('user_id');
-	var password=req.param('password');
-	var grade=req.param('grade');
-	var permission=req.param('permission');
-	var limit=req.param('limit');
-	User.create({name:name, user_id:user_id, password:password, grade:grade, permission:permission, limit:limit}).exec(function(err){});
+		var user_id=req.param('user_id');
+		var password=req.param('password');
+		var grade=req.param('grade');
+		var permission=req.param('permission');
+		var limit=req.param('limit');
+    var balance=0;
+		User.create({name:name, user_id:user_id, password:password, grade:grade, permission:permission, balance:balance, limit:limit}).exec(function(err){});
   	return res.redirect("/user");
   }
 };
