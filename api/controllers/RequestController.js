ceb4a7f9ac204de7847e1f1be1c63a45217f6f16
MYAX <myaxsan@gmail.com>
2014-06-16 19:00:09 +0900

    リクエスト検索(review)完成
    review検索を作ってもらいました

--- api/controllers/RequestController.js
+++ api/controllers/RequestController.js
@@ -8,13 +8,27 @@
 module.exports = {
 	
   index:function(req, res){
-		Request.find({}).exec(function(err,found){
-			this.requests = found;
-		});
+		var checkview = req.param('grade') || '';
+		if(checkview instanceof Array){
+			for(var i=0;i<checkview.length;i++){
+				checkview[i]={review:checkview[i]};
+			}
+		}
+		else{
+			if( checkview!== ""){
+				checkview=[{review:checkview}];
+			}
+			else{
+				checkview=[{}];
+			}
+		}
+		Request.find({or:checkview}).exec(function findCB(err,found){
+		this.requests = found;
+	});
     return res.view();
   },
   request:function(req, res){
-		Request.find({}).exec(function(err,found){
+		Request.find({}).exec(function (err,found){
 			this.requests = found;
 		});
 	  return res.view();
