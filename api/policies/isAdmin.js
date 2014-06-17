40b3c8a3a4931a74581a728d643189990a6da6db
makruk <tetsumasax@hotmail.com>
2014-06-18 00:55:46 +0900

    add Admin policie

--- api/policies/isAdmin.js
+++ api/policies/isAdmin.js
@@ -1,12 +1,7 @@
 module.exports = function(req, res, next) {
-<<<<<<< HEAD
   if (req.session.permission === 1) {
-=======
-  if (req.session.permission === "1") {
->>>>>>> make Policie
     return next();
   }
 
   return res.forbidden('You are not permitted to perform this action.');
 };
-
