f5f213c08f51afea9867ba6f8ae32f3b19feb2e2
MYAX <myaxsan@gmail.com>
2014-06-17 20:03:44 +0900

    リクエスト個別

--- api/controllers/RequestController.js
+++ api/controllers/RequestController.js
@@ -41,14 +41,27 @@ module.exports = {
     return res.view();
   },
 
-  request:function(req, res){
+	request:function(req,res){
+			var id = req.param('id');
+		Request.findOne({id:id}).exec(function (err,found){
+			this.request = found;
+		});
+		return res.view();
+	},
+
+	feedsubmit:function(req,res){
+		return res.redirect("/request");
+	},
+
+	/*以下購入者側*/
+  user_request:function(req, res){
 		Request.find({}).exec(function (err,found){
 			this.requests = found;
 		});
 	  return res.view();
   },
 
-	submit:function(req,res){
+	reqsubmit:function(req,res){
 		var request = req.param('request');
 		Request.create({request:request,review:0})
 		.exec(function(err){});
