d419d1284f45d2be8739a8b1430224dbc6170c89
makruk <tetsumasax@hotmail.com>
2014-06-17 17:41:24 +0900

    update route
    request

--- config/routes.js
+++ config/routes.js
@@ -47,6 +47,9 @@ module.exports.routes = {
   'get /stock/:id':'StockController.stock',
   'get /stock/:id/edit':'StockController.edit'
 
+  'get /request/:id':'RequestController.request',
+  'post /request/:id':'RequestController.feedback',
+
   // If a request to a URL doesn't match any of the custom routes above, it is matched 
   // against Sails route blueprints.  See `config/blueprints.js` for configuration options
   // and examples.
