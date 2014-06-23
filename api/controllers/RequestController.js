1f08358deddfa880aaf7aa08fa4baeaca2b050ba
MYAX <myaxsan@gmail.com>
2014-06-23 18:13:17 +0900

    フィードバックかんせい
    フィードバック部分が完成しました

--- api/controllers/RequestController.js
+++ api/controllers/RequestController.js
@@ -41,6 +41,7 @@ module.exports = {
     return res.view();
   },
 
+	/*管理者側、リクエスト詳細ページ*/
 	request:function(req,res){
 			var id = req.param('id');
 		Request.findOne({id:id}).exec(function (err,found){
@@ -48,13 +49,24 @@ module.exports = {
 		});
 		return res.view();
 	},
-
+	/*フィードバック送信 修正*/
 	feedsubmit:function(req,res){
-		return res.redirect("/request");
+		var id = req.param('id');
+		var newfeedback = req.param('feedback');
+		var newreview = req.param('r1');
+		Request.update({id:id},{responce:newfeedback, review:newreview}).exec(function(err,updated){
+        if(err){
+          console.log(err);
+          return res.redirect('/request/'+id);
+        }
+        else{
+          return res.redirect('/request');
+        }
+		});
 	},
 
 	/*以下購入者側*/
-  user_request:function(req, res){
+  user_request:function(req,res){
 		Request.find({}).exec(function (err,found){
 			this.requests = found;
 		});
@@ -65,7 +77,7 @@ module.exports = {
 		var request = req.param('request');
 		Request.create({request:request,review:0})
 		.exec(function(err){});
-		return res.redirect("/request/request");
+		return res.redirect("/request/user_request");
 	}
 };
 
