961f3f723a5de84ed62dedd3de045b3239857276
chikyu <11t5088g@gmail.com>
2014-06-24 01:29:24 +0900

    updateの際、パスワードが暗号化されない不具合を修正

--- api/models/User.js
+++ api/models/User.js
@@ -38,6 +38,17 @@ module.exports = {
         next();
       });
     });
+  },
+  beforeUpdate:function(attrs, next){
+    var bcrypt=require('bcrypt');
+    bcrypt.genSalt(10, function(err, salt){
+      if(err)return next(err);
+      bcrypt.hash(attrs.password, salt, function(err, hash){
+        if(err)return next(err);
+        attrs.password=hash;
+        next();
+      });
+    });
   }
 };
 
