42dc59df19f88f6da328ec8772dceb9dc2de17c7
MYAX <myaxsan@gmail.com>
2014-06-27 17:11:38 +0900

    購入者側リクエストの検索部分完成
    バグあったらゆるしてください

--- api/controllers/RequestController.js
+++ api/controllers/RequestController.js
@@ -16,7 +16,6 @@ module.exports = {
 		}
 		if(req.param('r1') == '1'){
 			checkfeed = {'!':['']};
-
 		}
 
 		/*評価の状態に関して*/
@@ -34,10 +33,16 @@ module.exports = {
 				checkview=[{}];
 			}
 		}
-
+		
 		Request.find({or:checkview,responce:checkfeed}).exec(function findCB(err,found){
 			this.requests = found;
 		});
+		for(var i = 0; i < this.requests.length;i++){
+			var id= this.requests[i].User;
+			User.findOne({id:id}).exec(function find(err,found){
+				this.requests[i].user_name = found.name;
+			});
+		}
     return res.view();
   },
 
@@ -46,6 +51,10 @@ module.exports = {
 			var id = req.param('id');
 		Request.findOne({id:id}).exec(function (err,found){
 			this.request = found;
+		});
+
+		User.findOne({id:this.request.User}).exec(function(err,found){
+			this.user = found;
 		});
 		return res.view();
 	},
@@ -67,15 +76,39 @@ module.exports = {
 
 	/*以下購入者側*/
   user_request:function(req,res){
-		Request.find({}).exec(function (err,found){
-			this.requests = found;
+		var user_id = req.session.user_id;
+		var id;
+		User.findOne({user_id:user_id}).exec(function(err,found){
+			id = found.id;
 		});
+		/*フィードバックのチェック*/
+		var checkfeed = {contains:""};
+		if(req.param('r2') == '1'){
+			checkfeed = {'!':['']};
+		}
+
+		/*自分の投稿のチェックをif文で検索*/
+		if(req.param('r1') == '1'){
+			Request.find({User:id,responce:checkfeed}).exec(function (err,found){
+				this.requests = found;
+			});
+		}else{
+			Request.find({responce:checkfeed}).exec(function (err,found){
+				this.requests = found;
+			});
+		}
 	  return res.view();
   },
 
 	reqsubmit:function(req,res){
 		var request = req.param('request');
-		Request.create({request:request,review:0})
+		var user_id = req.session.user_id;
+
+		var id;
+		User.findOne({user_id:user_id}).exec(function(err,found){
+			id = found.id;
+		});
+		Request.create({request:request,review:0,User:id})
 		.exec(function(err){});
 		return res.redirect("/request/user_request");
 	}
