77b363e5272ac74ccf4e66b2fc428499ee00f237
makruk <tetsumasax@hotmail.com>
2014-06-27 19:39:59 +0900

    add logging function

--- api/models/UserLog.js
+++ api/models/UserLog.js
@@ -12,8 +12,22 @@ module.exports = {
     kind:{type:"integer"},
     note:{type:"string"},
     user:{
-      model:"User"
+      model:"User",
+    },
+  },
+
+  //static method
+  addLog: function(user, cash, kind, note){
+    if(typeof user === "string"){
+      User.findOne({user_id:user}).exec(function(err, f){
+        user=f.id;
+      });
     }
+    UserLog.create({cash:cash, kind:kind, note:note, user:user}).exec(function(err, add){
+      if(err){
+        UserLog.create({cash:0, kind:0, note:"Error occured!! cash=["+cash+"]kind=["+kind+"]user=["+user+"]"})
+      }
+    });
   }
 };
 
