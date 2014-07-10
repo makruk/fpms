e222e5aa36d5431f7266ca753cb2b29bf58c720e
makruk <tetsumasax@hotmail.com>
2014-07-10 22:48:04 +0900

    ip

--- api/policies/isSecure.js
+++ api/policies/isSecure.js
@@ -1,11 +1,16 @@
 module.exports = function(req, res, next) {
+  var ipAddr = req.headers["x-forwarded-for"];
+  if (ipAddr){
+    var list = ipAddr.split(",");
+    ipAddr = list[list.length-1];
+  } else {
+    ipAddr = req.connection.remoteAddress;
+  }
+
+
   var part=req.ip.split(".");
   if(part[0] === "160" && part[1] === "252"){
     return next();
   }
-  var s="";
-  for(var i=0;i<req.ips.length;i++){
-    s+="["+req.ips[i]+"]";
-  }
-  return res.forbidden(req.ip+'You can access only from secure-net.'+s);
+  return res.forbidden(req.ip+'You can access only from secure-net.'+ipAddr);
 };
