8557dc3278852b9e79b842a880b1ab16d310c15c
makruk <tetsumasax@hotmail.com>
2014-07-10 16:46:30 +0900

    view ip test

--- api/policies/isSecure.js
+++ api/policies/isSecure.js
@@ -1,8 +1,11 @@
 module.exports = function(req, res, next) {
   var part=req.ip.split(".");
-  if(req.ip==="127.0.0.1")return next();
   if(part[0] === "160" && part[1] === "252"){
     return next();
   }
-  return res.forbidden(part[0]+"."+part[1]+"."+part[2]+"."+part[3]+'You can access only from secure-net.');
+  var s="";
+  for(var i=0;i<req.ips.length;i++){
+    s+="["+req.ips[i]+"]";
+  }
+  return res.forbidden(req.ip+'You can access only from secure-net.'+s);
 };
