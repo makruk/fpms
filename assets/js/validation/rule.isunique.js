7ca3eadd838f002e5c3b5c9a92cd1afef2bab67f
makruk <tetsumasax@hotmail.com>
2014-07-13 21:01:56 +0900

    hotfix. ignore current user_id

--- assets/js/validation/rule.isunique.js
+++ assets/js/validation/rule.isunique.js
@@ -1,20 +1,23 @@
 window.isUnique=function(field, rules, i, options){
   var responseText="";
-  $.ajax({
-    type:"GET",
-    url:"/user/isUnique/"+field.val(),
-    async:false,
-    dataType:'json',
-    success:function(data){
-      if(data.unique === false){
-        responseText="ユーザーIDが重複しています";
+  var ignore=rules[i+2];
+  if(ignore !== field.val()){
+    $.ajax({
+      type:"GET",
+      url:"/user/isUnique/"+field.val(),
+      async:false,
+      dataType:'json',
+      success:function(data){
+        if(data.unique === false){
+          responseText="ユーザーIDが重複しています";
+        }
+      },
+      error:function(){
+        responseText="サーバーエラー";
       }
-    },
-    error:function(){
-      responseText="サーバーエラー";
+    });
+    if(responseText !== ""){
+      return responseText;
     }
-  });
-  if(responseText !== ""){
-    return responseText;
   }
 }
