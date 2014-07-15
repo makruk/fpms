fe0b10d79aebd5af59a8ed46a7bea9aaed2a91a9
makruk <tetsumasax@hotmail.com>
2014-07-15 13:27:55 +0900

    first passward was too long.

--- api/controllers/FirstUserCreateController.js
+++ api/controllers/FirstUserCreateController.js
@@ -10,13 +10,7 @@ module.exports = {
     User.find({}).exec(function(err, f){
       if(err)return res.serverError("DB error.");
       if(f.length>0)return res.forbidden();
-      var pass=(function(){
-        var ret="";
-        for(var i=0;i<10;i++){
-          ret+=Math.round( Math.random() * 1e15 ).toString(36);
-        }
-        return ret;
-      })();
+      var pass=Math.round( Math.random() * 1e15 ).toString(36);
       User.create({limit:0, balance:0, permission:1, name:"admin", user_id:"admin", password:pass}).exec(function(err, c){
         c.password=pass;
         res.json(c);
